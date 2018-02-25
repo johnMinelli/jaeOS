0a3f767666e4b2819886995fab268c045b747430
johnMinelli <giovanni.minelli3@gmail.com>
2018-02-25 10:54:06 +0100

    2 ore per commentare...

--- phase1/src/phase1.c
+++ phase1/src/phase1.c
@@ -5,12 +5,18 @@
 #include "../h/constants.h"
 
 
-pcb_t *pcbfree_h;  //  testa  della  lista dei  PCB  che  sono  liberi o  inutilizzati.
-pcb_t pcbFree_table[MAXPROC]; // array  di PCB
+pcb_t *pcbfree_h;
+pcb_t pcbFree_table[MAXPROC];
 semd_t *semdFree_h;
 semd_t semd_table[MAXSEMD];
 semd_t *semdhash[ASHDSIZE];
 
+/*________________________________________________PCB_________________________________________________*/
+
+/*
+ * Funzione ricorsiva per inizializzare la pcbFree_h in modo da contenere tutti gli elementi della
+ * pcbFree_table
+ */
 void initPcbs_scan(int i, pcb_t **p){
     if (i<MAXPROC) {
         *p = &pcbFree_table[i++];
@@ -27,14 +33,21 @@ void initPcbs() {
     initPcbs_scan(0, &pcbfree_h);
 }
 
+/*
+ * Inserisce il PCB puntato da p nella lista dei PCB liberi. Inseriamo in testa alla coda dato il
+ * minor peso computazionale della funzione
+ */
 void freePcb(pcb_t *p){
-    if (p != NULL){             //<----- inserimento in coda o in testa??
+    if (p != NULL){
         p -> p_next=pcbfree_h;
         pcbfree_h=p;
     }
 }
 
-//alloca un processo prendendo da quelli liberi pcbfree
+/*
+ * Alloca un processo rimuovendo un elemento dalla pcbFree ed inizializzando tutti i campi (NULL/0)
+ * e restituisce l’elemento rimosso. Nel caso non ci siano elementi in pcbFree restituisco NULL
+ */
 pcb_t *allocPcb(){
     if(pcbfree_h==NULL){
         return NULL;
@@ -52,7 +65,10 @@ pcb_t *allocPcb(){
     }
 }
 
-//inserisce p nella coda di PCB head secondo priority
+/*
+ * Inserisce l’elemento puntato da p nella coda dei processi puntata da head tenendo conto della priority
+ * [testa=priority++++       coda=priority----]
+ */
 void insertProcQ(pcb_t **head, pcb_t *p){
     if(p!= NULL){
         if(*head==NULL || (*head)-> priority <= p->priority){
@@ -64,12 +80,14 @@ void insertProcQ(pcb_t **head, pcb_t *p){
     }
 }
 
-//restituisce il primo PCB della coda di PCB head
+//Restituisce il primo PCB della coda di PCB (quindi la testa)
 pcb_t *headProcQ(pcb_t *head){
     return  head;
 }
 
-//rimuove il primo PCB dalla coda di PCB head
+/*
+ * Rimuove il primo elemento dalla coda di PCB e ritorna lo stesso elemento
+ */
 pcb_t *removeProcQ(pcb_t **head){
     if(*head != NULL ){
         pcb_t *temp = *head;
@@ -79,7 +97,9 @@ pcb_t *removeProcQ(pcb_t **head){
     return NULL;
 }
 
-//rimuove il processo p dalla coda di PCB head
+/*
+ * Ricerca e rimuove il PCB puntato da p dalla coda dei PCB
+ */
 pcb_t *outProcQ(pcb_t **head, pcb_t *p){
     if(!(*head==NULL || p==NULL)){
         if(*head==p){
@@ -92,6 +112,7 @@ pcb_t *outProcQ(pcb_t **head, pcb_t *p){
     return NULL;
 }
 
+//Richiama la funzione [fun] per ogni elemento della lista di PCB
 void forallProcQ(pcb_t *head, void fun(pcb_t *pcb, void *), void *arg) {
     if (head != NULL) {
         fun(head, arg);
@@ -99,9 +120,12 @@ void forallProcQ(pcb_t *head, void fun(pcb_t *pcb, void *), void *arg) {
     }
 }
 
-//___________________________________TREE__________________________________________________________________
+/*________________________________________________TREE________________________________________________*/
 
-//restituisce l'ultimo fratello di p
+/*
+ *  Funzione ausiliaria per ottenere l'ultimo elemento nella lista di figli un PCB
+ * (quindi l'ultimo fratello del PCB puntato da p)
+ */
 pcb_t *lastSib(pcb_t *p){
     if(p->p_sib != NULL){
         return lastSib(p->p_sib);
@@ -109,7 +133,9 @@ pcb_t *lastSib(pcb_t *p){
     return p;
 }
 
-//inserisce p tra i figli di parent
+/*
+ *  Inserisce il PCB puntato da p in fondo alla lista dei figli del PCB puntato da parent.
+ */
 void insertChild(pcb_t *parent, pcb_t *p){
     p -> p_parent = parent;
     if(parent -> p_first_child == NULL){
@@ -119,7 +145,9 @@ void insertChild(pcb_t *parent, pcb_t *p){
     }
 }
 
-//rimuove il primo figlio di p
+/*
+ *  Rimuove il primo figlio del PCB puntato da p
+ */
 pcb_t *removeChild(pcb_t *p){
     if(p ->p_first_child != NULL){
         pcb_t *temp = p ->p_first_child;
@@ -131,7 +159,9 @@ pcb_t *removeChild(pcb_t *p){
     return NULL;
 }
 
-//rimuove (p) un PCB dalla lista dei figli di suo padre
+/*
+ *  Funzione ricorsiva per rimuovere il PCB puntato da p dalla lista dei figli del PCB puntato da parent
+ */
 pcb_t* outChild_scan(pcb_t **head, pcb_t* p){
     if(!(*head==NULL || p==NULL)){
         if(*head==p){
@@ -153,10 +183,12 @@ pcb_t *outChild(pcb_t* p){
     return NULL;
 }
 
-//___________________________________SEMAPHORES__________________________________________________________________
+/*________________________________________________SEMD________________________________________________*/
 
-
-//ausiliarie
+/*
+ *  Funzione ausiliaria che inserisce il semaforo s in fondo alla lista di semafori di cui head è
+ *  la testa.
+ */
 void setSemd(semd_t **head,semd_t *s) {
     if(*head==NULL){
         *head=s;
@@ -165,6 +197,11 @@ void setSemd(semd_t **head,semd_t *s) {
     }
 }
 
+/*
+ *  Funzione ausiliaria che restituisce il semaforo con la chiave indicata, cercandolo nella lista di
+ *  semafori di cui head è la testa. Tale lista è contenuta all'interno di una riga PREDETERMINATA
+ *  dell'hashtable di semafori (semdhash)
+ */
 semd_t *getSemd(semd_t *head,int *key) {
     if (head != NULL) {
         if (head->s_key == key) {
@@ -175,6 +212,10 @@ semd_t *getSemd(semd_t *head,int *key) {
     return NULL;
 }
 
+/*
+ *  Funzione ausiliaria per inserire il PCB puntato da p in fondo alla coda dei processi bloccati
+ *  [a differenza di 'insertProcQ()' qui non tengo conto della priority]
+ */
 void insertPtoSemQ(pcb_t *head, pcb_t *p){
     if(p!= NULL){
         if(head->p_next==NULL){
@@ -185,36 +226,43 @@ void insertPtoSemQ(pcb_t *head, pcb_t *p){
     }
 }
 
-//inserisce un PCB nella coda dei bloccati del semaforo con key
+/*
+ * Viene inserito il PCB puntato da p nella coda dei processi bloccati associata al semaforo con
+ * chiave  key.(1)
+ * (2)SE il semaforo con chiave key non esiste -> ne alloco ed inizializzo uno attingendo dalla semdFree
+ * (3)SE la semdFree è vuota -> restituisce -1
+ */
 int insertBlocked(int *key, pcb_t *p) {
-    int n = ((int)(key)/(int)sizeof(int))%ASHDSIZE;
-    semd_t *find = getSemd(semdhash[n],key);    //((int)(&sem[70])/(int)sizeof(int))%8
-    if(find != NULL) {
+    int n = ((int)(key)/(int)sizeof(int))%ASHDSIZE;     //determino la riga dell'hashtable
+    semd_t *find = getSemd(semdhash[n],key);
+    if(find != NULL) {  //(1)
         p->p_semKey = key;
         insertPtoSemQ(find->s_procQ, p);
         return 0;
     }else {
-        if (semdFree_h != NULL) {/////
-            //tolgo dai liberi
+        if (semdFree_h != NULL) {   //(2)
+
             semd_t *temp = semdFree_h;
             semdFree_h = semdFree_h->s_next;
-            //inizializzo semaforo
+
             temp->s_next = NULL;
             temp->s_key = key;
-            temp->s_procQ = p;  // per ins il proc in coda invece di ---->  insertPtoSemQ(&(temp), p);
-            //setto il processo
+            temp->s_procQ = p;
+
             p->p_semKey = key;
-            //inserisco il semaforo negli usati
+
             setSemd(&semdhash[n],temp);
             return 0;
         }
-        return -1;
+        return -1;  //(3)
     }
 }
 
-//restituisce la testa della coda dei PCB bloccati dal semaforo con key
+/*
+ *  Rstituisce un puntatore al primo PCB bloccato dal semaforo con chiave key
+ */
 pcb_t *headBlocked(int *key) {
-    int n = ((int)(key)/(int)sizeof(int))%ASHDSIZE;
+    int n = ((int)(key)/(int)sizeof(int))%ASHDSIZE;     //determino la riga dell'hashtable
     semd_t *find = getSemd(semdhash[n],key);
     if(find != NULL) {
         return find->s_procQ;
@@ -222,7 +270,11 @@ pcb_t *headBlocked(int *key) {
     return NULL;
 }
 
-//ausiliarie per free-are un semaforo con coda vuota
+/*
+ *  Funzione ausiliaria ricorsiva che restituisce il semaforo con la chiave indicata, cercandolo e
+ *  rimuovendolo dalla lista di semafori di cui head è la testa. Tale lista è contenuta all'interno
+ *  di una riga PREDETERMINATA dell'hashtable di semafori (semdhash)
+ */
 semd_t *removeSemd_scan(semd_t **head,int *key) {
     if (*head != NULL) {
         if((*head) ->s_key == key){
@@ -236,39 +288,44 @@ semd_t *removeSemd_scan(semd_t **head,int *key) {
     }
     return NULL;
 }
-
+// Funzione ausiliaria per liberare un semaforo
 void removeSemd(int *key){
-    int n = ((int)(key)/(int)sizeof(int))%ASHDSIZE;
-    setSemd(&semdFree_h,removeSemd_scan(&semdhash[n],key)); //mette tra i liberi quello che rimuove
+    int n = ((int)(key)/(int)sizeof(int))%ASHDSIZE;         //determino la riga dell'hashtable
+    setSemd(&semdFree_h,removeSemd_scan(&semdhash[n],key)); //inserisce tra i liberi il semd rimosso dall'hashtable
 }
 
-
-//rimuove il PCB in testa alla coda dei bloccati del semaforo che ha key
+/*
+ *  Rimuove e restituisce il primo PCB dalla coda dei processi bloccati dal semaforo con chiave key
+ *  SE la coda dei processi bloccati dal semaforo diventa vuota, libero il semaforo
+ *  [fuori dall'hashtable, dentro semdFree]
+ */
 pcb_t* removeBlocked(int *key){
-    pcb_t *q = headBlocked(key); //coda dei processi del semaforo con chiave key
+    pcb_t *q = headBlocked(key);    //puntatore alla testa della coda dei PCB bloccati dal semaforo con chiave key
     if(q != NULL){
-        pcb_t *out = removeProcQ(&q); //null se non rimuove niente(impossibile) altrimenti puntatore al proc rimosso dalla coda dei bloccati
-        if(q == NULL){
-            //coda vuota allora il semaforo é inutilizzato
-            removeSemd(key);  //<-- se volete si può togliere un metodo lasciando solo  _scan
+        pcb_t *out = removeProcQ(&q);
+        if(q == NULL){  //coda vuota -> il semaforo é inutilizzato
+            removeSemd(key);
         }
         return out;
     }
     return NULL;
 }
 
-//rimuove il PCB puntato da p dalla coda del semaforo su cui è bloccato
+/*
+ *  Rimuove il PCB puntato da p dalla coda del semaforo su cui è bloccato
+ *  SE la coda dei processi bloccati dal semaforo diventa vuota, libero il semaforo
+ */
 void outChildBlocked(pcb_t *p){
     if(p != NULL){
-        pcb_t *q = headBlocked(p -> p_semKey);//null se non trova il semaforo (impossibile)
+        pcb_t *q = headBlocked(p -> p_semKey);  //puntatore alla testa della coda dei PCB bloccati
         outProcQ(&q,p);
-        if(q != NULL){
-            //coda vuota allora il semaforo é inutilizzato
-            removeSemd(p -> p_semKey);  //<-- se volete si può togliere un metodo lasciando solo  _scan
+        if(q == NULL){  //coda vuota -> il semaforo é inutilizzato
+            removeSemd(p -> p_semKey);
         }
     }
 
 }
+
 
 void forallBlocked(int *key, void fun(pcb_t *pcb, void *), void *arg) {
     pcb_t *temp = headBlocked(key);
@@ -277,6 +334,10 @@ void forallBlocked(int *key, void fun(pcb_t *pcb, void *), void *arg) {
     }
 }
 
+/*
+ *  Funzione ricorsiva per inizializzare la lista dei semdFree in modo da contenere tutti gli
+ *  elementi della semdTable
+ */
 void initASL_scan(int i, semd_t **s){
     if (i<MAXSEMD) {
         *s = &semd_table[i++];
